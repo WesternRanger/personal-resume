49b6c89cf93c13f38c0423b52ca4ff52db7da1db
WesternRanger <western_ranger@163.com>
2014-09-26 16:18:10 +0800

    加上了基本的js，c3还待改正

--- js/personal.js
+++ js/personal.js
@@ -1,18 +1,21 @@
-function addLoadEvent(func){
-    var oldonload = window.onload;
-    if(typeof window.onload != 'function'){
-        window.onload = func;
-    }else{
-        window.onload = function(){
-            oldonload();
-            func();
-        }
+/**
+ * Created by 陈奇 on 14-9-26.
+ */
+function _load(){
+    var resu = document.getElementsByClassName("resu")[0],
+        na = document.getElementsByClassName("na")[0],
+        web = document.getElementsByClassName("web")[0];
+    resu.addEventListener("mouseover",down,false);
+    resu.addEventListener("mouseout",up,false);
+    resu.attachEvent("mouseover",down);
+    resu.attachEvent("mouseout",down);
+    function down(){
+        na.style.top = '50px';
+        web.style.top = '0';
+    }
+    function up(){
+        na.style.top = '0';
+        web.style.top = '50px';
     }
 }
-addLoadEvent(_load());
-function _load(){
-    test();
-}
-function test(){
-    // alert("asdf");
-}
+addLoadEvent(_load);
